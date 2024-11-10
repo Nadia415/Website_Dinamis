84cebcf54172cef2085a63c6d72216a094918a94
nadia permata putri <nadiapermataputri807@gmail.com>
2024-11-10 21:20:25 +0700

    initial files

---
+++ includes/contact.php
@@ -0,0 +1,12 @@
+<form id="contact-form">
+    <label for="name">Nama:</label>
+    <input type="text" id="name" name="name" required>
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required>
+
+    <label for="message">Pesan:</label>
+    <textarea id="message" name="message" required></textarea>
+
+    <button type="submit">Kirim Pesan</button>
+</form>
